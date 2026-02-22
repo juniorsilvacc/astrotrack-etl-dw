d5005aebd17aa757f4c7b8197697ab9d5b264e4e
juniorsilvacc <juniorsilvacc@hotmail.com>
2026-02-22 04:01:17 -0300

    feat: configurações inicias e conexão com banco de dados postgresql

---
+++ README.md
@@ -0,0 +1,5 @@
+# 🌟 Celestial: NASA Data Pipeline
+
+Este projeto de Engenharia de Dados consiste na construção de uma infraestrutura escalável para coleta, processamento, armazenar e visualizar eventos astronômicos e objetos próximos à Terra (NEOs). Utilizando APIs oficiais da NASA, o projeto integra dois fluxos de dados distintos: eventos de bólidos (bolas de fogo) e rastreamento de asteroides (NEOs).
+
+### `Em Construção...`
---
+++ src/drivers/postgres_driver.py
@@ -0,0 +1,47 @@
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import logging
+import time
+import os
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+class PostgreDriver:
+    def __init__(self):
+        load_dotenv()
+        
+        self.user = os.getenv("DB_USER")
+        self.password = os.getenv("DB_PASSWORD")
+        self.host = os.getenv("DB_HOST")
+        self.port = os.getenv("DB_PORT", "5432")
+        self.db_name = os.getenv("DB_NAME")
+        
+        self.engine = None
+        self._url = f"postgresql+psycopg2://{self.user}:{self.password}@{self.host}:{self.port}/{self.db_name}"
+
+    def connect(self):
+        """Estabelece a conexão com o banco de dados."""
+        if self.engine is None:
+            for i in range(1, 6):
+                try:
+                    temp_engine = create_engine(self._url)
+                    with temp_engine.connect() as conn:
+                        logging.info("Conexão com o PostgreSQL estabelecida com sucesso!")
+                    self.engine = temp_engine
+                    return self.engine
+                    
+                except SQLAlchemyError as e:
+                    logging.error(f"Tentativa {i}/5 - Erro ao conectar: {e}")
+                    time.sleep(3)
+            raise Exception("Não foi possível conectar ao banco de dados após várias tentativas.")
+        return self.engine
+
+    def get_engine(self):
+        """Retorna o engine, conectando se necessário."""
+        if not self.engine:
+            return self.connect()
+        return self.engine
